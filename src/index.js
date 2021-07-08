d880132962608bcdb3e3f8697325720ab0dd917a
cpan1995 <cpan1995@gmail.com>
2021-07-08 10:22:33 -0400

    completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
+    fetch("http://localhost:3000/dogs")
+    .then(response => response.json())
+    .then(data => {
+        createTable(data)
+    });
 
+    
+    
 })
+
+function createTable(data){
+    data.forEach(element => {
+        let trTag = document.createElement('tr');
+        let dogName = document.createElement('td');
+        let dogBreed = document.createElement('td');
+        let dogGender = document.createElement('td');
+        let editButton = document.createElement('button');
+        
+        dogName.textContent = element.name;
+        dogBreed.textContent = element.breed;
+        dogGender.textContent = element.sex;
+        dogName.id = 'nameTag';
+        dogBreed.id = 'breedTag'
+        dogGender.id = 'genderTag'
+        trTag.id = element.name;
+
+        let formData = {
+            id: element.name,
+            dogName: element.name,
+            dogBreed: element.breed,
+            dogGender: element.sex
+        }
+
+        editButton.textContent = "Edit"
+        editButton.addEventListener('click', () => {
+            fillFormBox(formData);
+        })
+
+        trTag.append(dogName, dogBreed, dogGender, editButton);
+        document.querySelector('#table-body').append(trTag);
+        
+    })
+}
+
+function fillFormBox(data){
+    let dogForm = document.querySelector("#dog-form");
+    dogForm.name.value = data.dogName;
+    dogForm.breed.value = data.dogBreed;
+    dogForm.sex.value = data.dogGender;
+
+    dogForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let currentRow = document.querySelector(`#${data.id}`)
+        let currentName = currentRow.querySelector('#nameTag');
+        let currentBreed = currentRow.querySelector('#breedTag');
+        let currentGender = currentRow.querySelector('#genderTag');
+
+        currentName.textContent = e.target.name.value;
+        currentBreed.textContent = e.target.breed.value;
+        currentGender.textContent = e.target.sex.value;
+
+        console.log(document.querySelector(`#${data.id}`).querySelector('#nameTag'));
+        console.log(document.querySelector(`#${data.id}`))
+    })
+}
