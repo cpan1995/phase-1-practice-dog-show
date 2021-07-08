da933b7f81b3eb0d7982df40dc523bd10442472d
cpan1995 <cpan1995@gmail.com>
2021-07-08 14:37:25 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+let formTracker;
+
 document.addEventListener('DOMContentLoaded', () => {
     fetch("http://localhost:3000/dogs")
     .then(response => response.json())
     .then(data => {
-        createTable(data)
+        createTable(data);
     });
-
+    document.querySelector('#dog-form').addEventListener('submit', (e)=> {
+        e.preventDefault();
+        currentRow = updateForm();
+        let currentName = currentRow.querySelector('#nameTag');
+        let currentBreed = currentRow.querySelector('#breedTag');
+        let currentGender = currentRow.querySelector('#genderTag');
+        currentName.textContent = e.target.name.value;
+        currentBreed.textContent = e.target.breed.value;
+        currentGender.textContent = e.target.sex.value;
+        console.log(e.target.name.value)
     
-    
+    })
 })
-
 function createTable(data){
-    data.forEach(element => {
+    data.forEach((element,index) => {
+        
         let trTag = document.createElement('tr');
         let dogName = document.createElement('td');
         let dogBreed = document.createElement('td');
@@ -24,19 +35,17 @@ function createTable(data){
         dogBreed.id = 'breedTag'
         dogGender.id = 'genderTag'
         trTag.id = element.name;
-
+        editButton.textContent = "Edit"
         let formData = {
-            id: element.name,
             dogName: element.name,
             dogBreed: element.breed,
             dogGender: element.sex
         }
-
-        editButton.textContent = "Edit"
         editButton.addEventListener('click', () => {
             fillFormBox(formData);
+            formTracker = index;
         })
-
+        
         trTag.append(dogName, dogBreed, dogGender, editButton);
         document.querySelector('#table-body').append(trTag);
         
@@ -48,19 +57,14 @@ function fillFormBox(data){
     dogForm.name.value = data.dogName;
     dogForm.breed.value = data.dogBreed;
     dogForm.sex.value = data.dogGender;
-
-    dogForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let currentRow = document.querySelector(`#${data.id}`)
-        let currentName = currentRow.querySelector('#nameTag');
-        let currentBreed = currentRow.querySelector('#breedTag');
-        let currentGender = currentRow.querySelector('#genderTag');
-
-        currentName.textContent = e.target.name.value;
-        currentBreed.textContent = e.target.breed.value;
-        currentGender.textContent = e.target.sex.value;
-
-        console.log(document.querySelector(`#${data.id}`).querySelector('#nameTag'));
-        console.log(document.querySelector(`#${data.id}`))
+    
+}
+function updateForm(){
+    let currentRow;
+    document.querySelector('#table-body').childNodes.forEach((element, index) => {
+        if (index == formTracker + 1){
+            currentRow = element;
+        }
     })
+    return currentRow
 }
